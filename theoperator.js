9a49c67dce866bf5f20c564ced4029efeb0ab22d
Michael Heilemann <mheilemann@squarespace.com>
2014-04-07 22:18:49 -0400

    Better save this somewhere.

---
+++ theoperator.js
@@ -0,0 +1,37 @@
+var reloadTimer;
+
+function init () {
+  // reloadOnChange(chrome.extension.getURL('/theoperator.js'));
+  // reloadOnChange(chrome.extension.getURL('/manifest.json'));
+  reloadOnChange(chrome.extension.getURL('/mystyles.css'));
+}
+
+function reloadOnChange (url, checkIntervalMS) {
+    if (!window.__watchedFiles) {
+        window.__watchedFiles = {};
+    }
+
+    (function() {
+        var self = arguments.callee;
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if (__watchedFiles[url] &&
+                    __watchedFiles[url] != xhr.responseText) {
+                    clearTimeout(reloadTimer);
+                    chrome.runtime.reload();
+                    init();
+                } else {
+                    __watchedFiles[url] = xhr.responseText
+                    window.setTimeout(self, checkIntervalMS || 1000);
+                }
+            }
+        };
+
+        xhr.open("GET", url, true);
+        xhr.send();
+    })();
+}
+
+init();
